2f0982d7f782f0544a304b6958a184b45b0bed29
RaulORios <raul.orios89@outlook.com>
2021-08-10 13:28:41 -0300

    Arqwuivos adicionados

---
+++ Uri_1044_ok/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Uri_1044_ok
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] vet = Console.ReadLine().Split(' ');
+            int a = int.Parse(vet[0]);
+            int b = int.Parse(vet[1]);
+
+            if (a % b == 0 || b % a == 0)
+            {
+                Console.WriteLine("São Multiplos");
+            }
+            else
+            {
+                Console.WriteLine("Não são Multiplos");
+            }
+        }
+    }
+}
